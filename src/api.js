36e2130f879cd251e6fb1063e0083679f010601f
Arasavalli chandra sekhar <chanduarasavalli95@gmail.com>
2025-10-05 10:42:09 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api';
+const API_BASE = 'https://bookreviewsbackend.onrender.com';
 
 // Function to get JWT token from localStorage
 const getToken = () => localStorage.getItem('token');
@@ -19,3 +19,4 @@ client.interceptors.request.use(config => {
 });
 
 export default client;
+
